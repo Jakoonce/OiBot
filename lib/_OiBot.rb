6298805954446a2d10a14c3fd55772ac16f68300
Joseph Koonce <jakoonce00100@gmail.com>
2019-06-26 13:03:30 -0400

    added custom keyboard to weather feature

--- lib/_OiBot.rb
+++ lib/_OiBot.rb
@@ -19,27 +19,30 @@ class OiBot
         case message.text
         when '/start'
           bot.api.send_message(chat_id: message.chat.id, text:'Hello, #{message.from.first_name}, here is what I can do for you:
-            /todayWeather
-            /weekWeather')
+            /weather')
 
-        when '/todayWeather'
-          #pull out only the data for today and send it
-          current_summary = @forecast['currently']
-          bot.api.send_message(chat_id: message.chat.id, text: "today is #{current_summary["temperature"]}F and #{(current_summary['summary']).downcase}.\nprobability of rain: #{((current_summary['precipProbability']) * 100)}%\n ")
-        
-        when '/weekWeather'  
-          @forecast["daily"].each do |key, value|
-            if key == "data"
-              value.each do |n|
-                date = Time.at(n['time'])
-                bot.api.send_message(chat_id: message.chat.id, text: "#{date.month}/#{date.day}\nprobability of rain: #{((n['precipProbability']) * 100)}%\nHigh: #{n['temperatureMax']}F\tLow: #{n['temperatureMin']}F\n#{n['summary']}")
+        when '/weather'
+          question = 'Would you like the weather for right now or the week?'
+              answers =
+                Telegram::Bot::Types::ReplyKeyboardMarkup
+                .new(keyboard: [%w(Now), %w(Week)], one_time_keyboard: true)
+                bot.api.send_message(chat_id: message.chat.id, text: question, reply_markup: answers)
+
+        when 'Now'
+              #pulls out only the data for today and sends it
+              current_summary = @forecast['currently']
+              bot.api.send_message(chat_id: message.chat.id, text: "today is #{current_summary["temperature"]}F and #{(current_summary['summary']).downcase}.\nprobability of rain: #{((current_summary['precipProbability']) * 100)}%\n ")
+        when 'Week'
+          # loops through the daily hash from the weather api and sends each day
+              @forecast["daily"].each do |key, value|
+                if key == "data"
+                  value.each do |n|
+                    date = Time.at(n['time'])
+                    bot.api.send_message(chat_id: message.chat.id, text: "#{date.month}/#{date.day}\nprobability of rain: #{((n['precipProbability']) * 100)}%\nHigh: #{n['temperatureMax']}F\tLow: #{n['temperatureMin']}F\n#{n['summary']}")
+                  end
+                end
               end
             end
-          end
-
-        when
-
-        end
 
       end
 
